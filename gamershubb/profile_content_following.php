3dba9ba28cfb2e63f5fde8906e1a10376667cb81
TerenceBorromeo <72066115+TerenceBorromeo@users.noreply.github.com>
2022-01-21 03:34:59 +0400

    Create profile_content_following.php

---
+++ gamershubb/profile_content_following.php
@@ -0,0 +1,30 @@
+<br><br><br>
+<div style="min-height: 400px;width:100%;background-color: #1a1a1a;text-align: center;"><br>
+<h1 style="text-align: center; color: white;"><?php echo ($user_data['username'])?>'s Buddies</h1>
+	<div style="padding: 20px;">
+	<?php
+ 
+		$image_class = new Image();
+		$post_class = new Post();
+		$user_class = new User();
+
+		$following = $user_class->get_following($user_data['userid'],"user");
+
+		if(is_array($following)){
+
+			foreach ($following as $follower) {
+				# code...
+				$FRIEND_ROW = $user_class->get_user($follower['userid']);
+				include("user.php");
+			}
+
+		}else{
+
+			echo "This user inst following anyone!";
+		}
+
+
+	?>
+
+	</div>
+</div>
