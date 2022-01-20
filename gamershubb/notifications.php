290efdf24b9568ff69d494ce42f1fd9dad1c683d
TerenceBorromeo <72066115+TerenceBorromeo@users.noreply.github.com>
2022-01-21 03:32:11 +0400

    Create notifications.php

---
+++ gamershubb/notifications.php
@@ -0,0 +1,78 @@
+<?php 
+
+
+
+	$login = new Login();
+	$user_data = $login->check_login($_SESSION['mybook_userid']);
+ 
+ 	$USER = $user_data;
+ 	
+ 	if(isset($_GET['id']) && is_numeric($_GET['id'])){
+
+	 	$profile = new Profile();
+	 	$profile_data = $profile->get_profile($_GET['id']);
+
+	 	if(is_array($profile_data)){
+	 		$user_data = $profile_data[0];
+	 	}
+
+ 	}
+	
+	$Post = new Post();
+	$User = new User();
+ 	$image_class = new Image();
+
+?>
+		<!--cover area-->
+		<div style="width: 400px;min-height: 400px;">
+		 
+			<!--below cover area-->
+			<div style="display: flex;">	
+
+				<!--posts area-->
+ 				<div style="min-height: 400px;flex:2.5;padding: 20px;padding-right: 0px;">
+ 					
+ 					<div style="padding: 10px;background-color: #222;">
+
+ 						<?php 
+
+ 							$DB = new Database();
+ 							$id = esc($_SESSION['mybook_userid']);
+ 							$follow = array();
+
+ 							//check content i follow
+ 							$sql = "select * from content_i_follow where disabled = 0 && userid = '$id' limit 100";
+ 							$i_follow = $DB->read($sql);
+ 							if(is_array($i_follow)){
+ 								$follow = array_column($i_follow, "contentid");
+ 							}
+
+ 							if(count($follow) > 0){
+
+ 								$str = "'" . implode("','", $follow) . "'";
+   								$query = "select * from notifications where (userid != '$id' && content_owner = '$id') || (contentid in ($str)) order by id desc limit 30";
+ 							}else{
+
+  								$query = "select * from notifications where userid != '$id' && content_owner = '$id' order by id desc limit 30";
+ 							}
+ 
+ 							$data = $DB->read($query);
+ 						?>
+
+ 						<?php if(is_array($data)): ?>
+
+ 							<?php foreach ($data as $notif_row): 
+ 							 
+ 							 	include("single_notification.php");
+  					 		 endforeach; ?>
+  					 		<?php else: ?>
+  					 			No notifications were found
+  					 		<?php endif; ?>
+
+ 					</div>
+  
+
+ 				</div>
+			</div>
+
+		</div>
