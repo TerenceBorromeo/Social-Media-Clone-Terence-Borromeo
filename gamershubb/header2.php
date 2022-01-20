5f8c6db102ae21b4ad8ec20396fff9e2c206a118
TerenceBorromeo <72066115+TerenceBorromeo@users.noreply.github.com>
2022-01-21 03:31:00 +0400

    Create header2.php

---
+++ gamershubb/header2.php
@@ -0,0 +1,15 @@
+<div id="blue_bar">
+    &nbsp;&nbsp;&nbsp;&nbsp;
+    <a href="#" style="text-decoration: none;cursor: not-allowed;">
+    <i class="fas fa-angle-double-left" style="font-size: 50px; color: white;"></i>
+    </a>&nbsp;&nbsp;
+    <a href="#" style="text-decoration: none;cursor: not-allowed;">
+		<img src="images/ghlogo.png" width="45px" height="45px" style="margin-top: 2px; margin-left: 5px;"/>
+	</a>
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    <a href="#" style="text-decoration: none;cursor: not-allowed;">
+    <span>
+        <h1 style="font-family:'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif; font-size: 45px; color: white;">GamersHub</h1>
+    </span>
+    </a>
+</div>
